7c222bfd876604b5a55a42f0101a0d41be86d012
kwonhaejin <kwonhaejin1781@gmail.com>
2022-09-06 10:50:09 +0900

    실습

--- 20220906/20220906/star.c
+++ 20220906/20220906/star.c
@@ -4,24 +4,10 @@ int main()
 	int n = 0;
 
 	scanf_s("%d", &n);
-	for (int i = n; i > 0; i--)
+	
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = i; j > 0; j--)
-			printf("*");
-		for (int k = 2 * n - 2 * i; k > 0; k--)
-			printf(" ");
-		for (int l = i; l > 0; l--)
-			printf("*");
-		printf("\n");
-	}
-	for (int i = 1; i <= n; i++) {
-		for (int j = i; j > 0; j--)
-			printf("*");
-		for (int k = 2 * n - 2 * i; k > 0; k--)
-			printf(" ");
-		for (int l = i; l > 0; l--)
-			printf("*");
-		printf("\n");
+		printf("*");
 	}
 	
 	return 0;
